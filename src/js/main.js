865db0d141182c576c8501299eb81045347e5eff
VladGumeniuk <pendulumplay@gmail.com>
2024-06-18 14:22:51 +0300

    added new sections v2

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,15 @@ import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse'
 import {mobileMenu} from './store/mobile-menu';
 import {stopScroll} from './store/stop-scroll';
+import {sliderProduct} from './components/slider-product';
 
 
 
 
 document.addEventListener('alpine:init', () => {
     Alpine.plugin(collapse);
+
+    Alpine.data("sliderProduct", sliderProduct);
 
     Alpine.store("mobileMenu", mobileMenu);
     Alpine.store("stopScroll", stopScroll);

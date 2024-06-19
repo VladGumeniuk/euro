603eaf5206b60985ef04f45f3f68511022da6f75
VladGumeniuk <pendulumplay@gmail.com>
2024-06-19 14:16:21 +0300

    added page + timer

---
+++ src/js/components/timer.js
@@ -0,0 +1,32 @@
+export const timer = () => ({
+    days: null,
+    hours: null,
+    minutes: null,
+    seconds: null,
+
+    init() {
+        const countDownDate = new Date("June 30, 2024 00:00:00").getTime();
+        const updateCountdown = () => {
+            const now = new Date().getTime();
+            const distance = countDownDate - now;
+
+            this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance < 0) {
+                clearInterval(countdownFunction);
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            } else {
+                document.getElementById("days").innerHTML = this.days;
+                document.getElementById("hours").innerHTML = this.hours;
+                document.getElementById("minutes").innerHTML = this.minutes;
+                document.getElementById("seconds").innerHTML = this.seconds;
+            }
+        };
+
+        const countdownFunction = setInterval(updateCountdown, 1000);
+        updateCountdown(); // Initial call to set values immediately
+    },
+});

e8e6e1cc66827238121760bfc36d36e0ec8a9fd6
VladGumeniuk <pendulumplay@gmail.com>
2024-06-20 14:34:49 +0300

    added new pages how to praticipate

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -1,11 +1,14 @@
-import Swiper, {Navigation, Thumbs} from 'swiper';
+import Swiper, { Navigation, Thumbs } from 'swiper';
 
-Swiper.use([Thumbs , Navigation]);
+Swiper.use([Thumbs, Navigation]);
 
 const sliderOptions = {
     mainSlider: {
         slidesPerView: 1,
         autoHeight: true,
+        thumbs: {
+            swiper: null
+        },
     },
     mainSliderModal: {
         spaceBetween: 10,
@@ -27,14 +30,30 @@ const sliderOptions = {
 
 export const sliderSwiper = (sliderOption) => ({
     slider: null,
+    thumbsSlider: null,
 
     init() {
         document.addEventListener('DOMContentLoaded', () => {
-            this.initSwiper();
+            this.initThumbsSlider();
+            this.initMainSlider();
         });
     },
 
-    initSwiper() {
+    initThumbsSlider() {
+        if (sliderOption === 'mainSlider') {
+            this.thumbsSlider = new Swiper('.swiper-container-thumbs', sliderOptions.mainSliderModal);
+        }
+    },
+
+    initMainSlider() {
+        if (sliderOption === 'mainSlider') {
+            sliderOptions.mainSlider.thumbs.swiper = this.thumbsSlider;
+        }
+
         this.slider = new Swiper(this.$el, sliderOptions[sliderOption]);
     },
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    Alpine.data('sliderSwiper', sliderSwiper);
 });
